30d924f3896e1e4f11ed77d3c9cbefd5d964344d
Fellipe C.T De Camargo <fellipectcamargo@gmail.com>
2024-11-05 11:35:08 -0400

    Folder structure and main menu implemented

---
+++ comp2129_group_project/comp2129_group_project/Util/Util.cs
@@ -0,0 +1,24 @@
+﻿using static comp2129_group_project.Validation.Validation;
+
+namespace comp2129_group_project.Util
+{
+    public class Util
+    {
+        // Get user input and validate
+        public static string GetInput(int maxInputs)
+        {
+            try
+            {
+                string temp = Console.ReadLine()!;
+                string result = ValidateUserInput(temp, maxInputs);
+
+                return result;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return "";
+            }
+        }
+    }
+}

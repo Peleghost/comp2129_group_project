54feed36611e18e68d5f2aa6adedecb8a99b919d
leafschick <160258327+leafschick@users.noreply.github.com>
2024-11-18 04:43:56 -0500

    Update Customer.cs

--- comp2129_group_project/comp2129_group_project/Entities/Customer.cs
+++ comp2129_group_project/comp2129_group_project/Entities/Customer.cs
@@ -1,28 +1,42 @@
-﻿namespace comp2129_group_project.Entities
+namespace comp2129_group_project.Entities
 {
     public class Customer
     {
-        // Auto increment id for each new customer created
-        // on the shell instance
         private static int i = 0;
 
-        private int Id { get; set; } = ++i;
-        public int CustomerId { get; set; }
-        public string? FirstName { get; set; }
-        public string? LastName { get; set; }
+        private int Id { get; set; } = ++i; 
+        public int CustomerId => Id; 
+        public string? FirstName { get; private set; }
+        public string? LastName { get; private set; }
         public string? Phone { get; set; }
         public int NumOfBookings { get; set; }
 
-        public Customer()
-        {
-        }
+        
+        public Customer() { }
 
         public Customer(string firstName, string lastName, string phone)
         {
-            CustomerId = Id;
-            FirstName = firstName;
-            LastName = lastName;
+            FirstName = string.IsNullOrWhiteSpace(firstName) 
+                ? throw new ArgumentException("Sorry, but the First name cannot be empty.") 
+                : firstName;
+
+            LastName = string.IsNullOrWhiteSpace(lastName) 
+                ? "This is an Unknown" 
+                : lastName;
+
             Phone = phone;
+        }
+
+        // This will be validates if FirstName is not null or empty
+        public bool ValidateFirstName()
+        {
+            return !string.IsNullOrWhiteSpace(FirstName);
+        }
+
+        // this method will be used to display the full name
+        public override string ToString()
+        {
+            return $"{FirstName} {LastName}";
         }
     }
 }

e3ee616a800edbf06a1b0c378c28c076ebe19b02
leafschick <160258327+leafschick@users.noreply.github.com>
2024-11-18 05:24:48 -0500

    Update Program.cs

--- comp2129_group_project/comp2129_group_project/Program.cs
+++ comp2129_group_project/comp2129_group_project/Program.cs
@@ -1,4 +1,5 @@
-﻿using comp2129_group_project.Managers;
+using comp2129_group_project.Managers;
+using comp2129_group_project.Entities;
 using static comp2129_group_project.Display.Display;
 
 namespace comp2129_group_project
@@ -9,13 +10,15 @@ namespace comp2129_group_project
     // Members:
     //
     // Fellipe C.T.C - 101497831
-    // Ayesha Akbar --
+    // Ayesha Akbar 100949840
     // Claire Lee ----
     // Suthan S. -----
-    //
+    //                         
     //---------------------------------------------
     internal class Program
     {
+        private static CustomerManager customerManager = new CustomerManager(10); // Maximum 10 customers
+
         static void Main(string[] args)
         {
             //
@@ -35,23 +38,17 @@ namespace comp2129_group_project
                 {
                     // Customers
                     case "1":
-                        string input1 = MenuCustomers();
-                        if (input1 == "0")
-                        {
-                            userInput = MenuMain();
-                        }
-
-                        // TODO: Handle customer menu items
+                        HandleCustomerMenu();
+                        userInput = MenuMain();
                         break;
 
                     // Flights
                     case "2":
-                        string input2 = MenuFilghts();
+                        string input2 = MenuFlights();
                         if (input2 == "0")
                         {
                             userInput = MenuMain();
                         }
-
                         // TODO: Handle flights menu items
                         break;
 
@@ -62,7 +59,6 @@ namespace comp2129_group_project
                         {
                             userInput = MenuMain();
                         }
-
                         // TODO: Handle bookings menu items
                         break;
 
@@ -73,8 +69,42 @@ namespace comp2129_group_project
                         break;
 
                     default:
-                        Console.WriteLine("SWITCH CASE DEFAULT");
+                        Console.WriteLine("Sorry this is an invalid choice. Please try again.");
                         quit = true;
+                        break;
+                }
+            }
+        }
+
+        static void HandleCustomerMenu()
+        {
+            bool backToMainMenu = false;
+
+            while (!backToMainMenu)
+            {
+                string input = MenuCustomers();
+
+                switch (input)
+                {
+                    case "1": // Add a new customer
+                        customerManager.AddanewCustomer();
+                        break;
+
+                    case "2": // View customers
+                        customerManager.ViewCustomersInformation();
+                        break;
+
+                    case "3": // Delete a customer
+                        customerManager.DeleteCustomer();
+                        break;
+
+                    case "4": // Back to Main Menu
+                        backToMainMenu = true;
+                        break;
+
+                    default:
+                        Console.WriteLine("Sorry but this is an invalid choice. Please try again.");
+                        Console.ReadKey();
                         break;
                 }
             }
